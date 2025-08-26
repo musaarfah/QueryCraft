bcfc54847d05067f5871d18c9389ebb2a0ee6e7c
musaarfah <musaarfah2@gmail.com>
2025-08-26 17:57:58 +0500

    frontend changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,7 @@
 import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: undefined, // remove output/export entirely
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
+  
 };
 
 export default nextConfig;
