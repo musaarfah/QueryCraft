ee215fdce44f26eec879eafbe136605eea1b237c
musaarfah <musaarfah2@gmail.com>
2025-08-26 16:36:28 +0500

    changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   eslint: {
-    ignoreDuringBuilds: true, // 👈 disables ESLint errors from breaking build
+    ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true, // 👈 disables TS errors from breaking build
+    ignoreBuildErrors: true,
   },
 };
 
